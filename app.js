a20276d4a5a03d22bd38e37d1ff199508122f987
Arsentii Grygoriev <arsef1235@gmail.com>
2021-07-03 19:18:49 +0300

    completed edit-btn 2.0

--- app.js
+++ app.js
@@ -33,9 +33,8 @@ function addItem(event) {
     const newTodo = document.createElement('li');
     const date = new Date()
     let dates = (todoDates.value).split(',')
-    console.log(todoContent.value)
     /*newTodo.innerHTML='<form>'+'<div class="todoNameIcon">'+setIcon(todoCategory.value)+checkNull(todoName.value)+'</div>'+'<span class="todo_list_date">'+months[date.getMonth()]+' '+date.getDay()+','+date.getFullYear()+'</span>'+'<span class="todo_item_category">'+todoCategory.value+'</span>'+'<span class="todo_item_content">'+checkNull(todoContent.value)+'</span>'+'<span class="todo_list_dates>'+dates+'</span></form>';*/
-    newTodo.innerHTML='<form>'+'<span>'+setIcon(todoCategory.value)+checkNull(todoName.value)+'</span>'+'<p>'+months[date.getMonth()]+' '+date.getDay()+','+date.getFullYear()+'</p>'+'<span>'+todoCategory.value+'</span>'+'<span>'+checkNull(todoContent.value)+'</span>'+'<span>'+dates+'</span>'+'</form>';
+    newTodo.innerHTML='<form>'+'<span>'+setIcon(todoCategory.value)+checkNull(todoName.value)+'</span>'+'<span>'+months[date.getMonth()]+' '+date.getDay()+','+date.getFullYear()+'</span>'+'<span>'+todoCategory.value+'</span>'+'<span>'+checkNull(todoContent.value)+'</span>'+'<span>'+dates+'</span>'+'</form>';
     newTodo.classList.add('list_item_li')
     todoDiv.appendChild(newTodo);
     //edit button
@@ -56,7 +55,6 @@ function addItem(event) {
     //append to list
     todoList.appendChild(todoDiv)
     const todos = todoList.childNodes;
-    console.log(todos)
     //clear input values
     todoName.value =""
     todoContent.value =""
@@ -67,11 +65,11 @@ function getSelectedValue() {
     console.log(selectedValue)
 }
 function setIcon(value) {
-    console.log(value)
     if(value == 'qoute') return '<i class="fas fa-quote-right" id="qouteIcon"></i>'
     if(value == 'task') return '<i class="fas fa-thumbtack" id="taskIcon"></i>'
     if(value == 'idea') return '<i class="fas fa-lightbulb" id="ideaIcon"></i>'
     if(value == 'random thought') return '<i class="fas fa-head-side-virus" id="randomIcon"></i>'
+    if(value == 'select:') return '<i class="fas fa-exclamation-triangle"></i>'
 }
 function checkNull(value) {
     if(!value) return 'None';
@@ -87,36 +85,70 @@ function deleteCheck(e) {
         const form = todo.firstElementChild;
         const spanName = form.firstElementChild;
         const spans = form.childNodes;
-        console.log(spans)
         spans.forEach(function(currentValue,currentIndex,listObj){
             if(currentIndex==1) ++currentIndex;
             if(currentIndex==2){
                 const select = document.createElement('select');
-                let firstOption = new Option('Random Thought','random thought');
-                let secondOption = new Option("Idea","idea");
-                let thirdOption = new Option('Task','task');
-                let forthOption = new Option('Quote','quote');
-                let options = [firstOption,secondOption,thirdOption,forthOption];
-                for(let i=0; i<4; i++){
+                let firstOption = new Option('Select:','select:');
+                let secondOption = new Option('Random Thought','random thought');
+                let thirdOption = new Option("Idea","idea");
+                let forthOption = new Option('Task','task');
+                let fifthOption = new Option('Qoute','qoute');
+                let options = [firstOption,secondOption,thirdOption,forthOption,fifthOption];
+                for(let i=0; i<5; i++){
                 select.options[select.options.length]=options[i];
                 }
+                select.classList.add('edit_select');
                 form.insertBefore(select,spans[currentIndex]);
                 form.removeChild(spans[++currentIndex]);
             }
             const input = document.createElement('input');
             input.type = "text";
-            console.log(currentIndex);
             input.value=spans[currentIndex].textContent;
             input.classList.add('edit_input')
-            console.log(spans);
             form.insertBefore(input,spans[currentIndex]);
             form.removeChild(spans[++currentIndex])
         })
-           
-            const submitBtn = item.firstChild;
-            submitBtn.classList.toggle('fa-pencil-alt');
-            submitBtn.classList.toggle('fa-check');
-            item.classList.toggle('edit-btn');
-            item.classList.toggle('submit-btn');
+        const newSpans = form.childNodes;
+        newSpans[2].onchange=function(){
+            let selected = this.options[this.selectedIndex];
+            return selected.value;
+        }
+        const submitBtn = item.firstChild;
+        submitBtn.classList.toggle('fa-pencil-alt');
+        submitBtn.classList.toggle('fa-check');
+        item.classList.toggle('edit-btn');
+        item.classList.toggle('submit-btn');
+    } else if (item.classList[0]==='submit-btn'){
+        const form = todo.firstElementChild;
+        const spans = form.childNodes;
+        spans.forEach(function(currentValue,currentIndex,listObj){
+            const span = document.createElement('span');
+            if(currentIndex==0){
+            span.innerHTML=setIcon(spans[2].value)+spans[currentIndex].value;
+            form.insertBefore(span,spans[currentIndex]);
+            form.removeChild(spans[++currentIndex]);
+            }
+            if(currentIndex==1) {++currentIndex;
+            }else if(currentIndex==2){
+                span.textContent = spans[currentIndex].value;
+                form.insertBefore(span,spans[currentIndex]);
+                form.removeChild(spans[++currentIndex]);   
+                ++currentIndex;
+            }else {
+            span.textContent = currentValue.value;
+            form.insertBefore(span,spans[currentIndex]);
+            form.removeChild(spans[++currentIndex]);   
+            }
+        })
+        const submitBtn = item.firstChild;
+        submitBtn.classList.toggle('fa-check');
+        submitBtn.classList.toggle('fa-pencil-alt');
+        item.classList.toggle('submit-btn');
+        item.classList.toggle('edit-btn');
     }
+}
+function getSelectedValue(){
+    let selectedOption = this.options[this.selectedIndex];
+    alert(selectedOption.value);
 }
