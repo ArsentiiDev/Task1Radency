87dbf7cee16708d228e661c95f1693dc750e9b14
Arsentii Grygoriev <arsef1235@gmail.com>
2021-07-04 11:00:43 +0300

    added sum list

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const months = [
     'october',
     'november',
     'december',
-]
+];
+let activeCount = 0;
+let archiveCount =0;
 //selectors
 const todoName = document.querySelector('.add_todo_name');
 const todoContent = document.querySelector('.add_todo_content');
@@ -20,6 +22,7 @@ const todoButton = document.querySelector('.add_todo_btn');
 const todoList = document.querySelector('.todo_list');
 const todoCategory = document.querySelector('.category_list');
 const todoDates = document.querySelector('.add_todo_dates');
+const SumList = document.querySelector('.summary_list');
 
 //event listeners
 document.addEventListener('DOMContentLoaded',getTodos);
@@ -28,6 +31,7 @@ todoList.addEventListener('click', deleteEditArchive);
 //functions
 function addItem(event) {
     //prevent from submitting
+    activeCount++;
     event.preventDefault()
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('list_item_div')
@@ -37,12 +41,15 @@ function addItem(event) {
     let creationDate = getDate(date);
     let dates = (todoDates.value).split(',')
     /*newTodo.innerHTML='<form>'+'<div class="todoNameIcon">'+setIcon(todoCategory.value)+checkNull(todoName.value)+'</div>'+'<span class="todo_list_date">'+months[date.getMonth()]+' '+date.getDay()+','+date.getFullYear()+'</span>'+'<span class="todo_item_category">'+todoCategory.value+'</span>'+'<span class="todo_item_content">'+checkNull(todoContent.value)+'</span>'+'<span class="todo_list_dates>'+dates+'</span></form>';*/
-    newTodo.innerHTML='<form>'+'<span>'+setIcon(todoCategory.value)+checkNull(todoName.value)+'</span>'+'<span>'+creationDate+'</span>'+'<span>'+todoCategory.value+'</span>'+'<span>'+checkNull(todoContent.value)+'</span>'+'<span>'+dates+'</span>'+'</form>';
+    newTodo.innerHTML=`<form><span>${setIcon(todoCategory.value)} ${checkNull(todoName.value)}</span><span>${creationDate}</span><span>${todoCategory.value}</span><span>${checkNull(todoContent.value)}</span><span>${dates}</span></form>`;
+    //newTodo.innerHTML=<form>'+'<span>'+setIcon(todoCategory.value)+checkNull(todoName.value)+'</span>'+'<span>'+creationDate+'</span>'+'<span>'+todoCategory.value+'</span>'+'<span>'+checkNull(todoContent.value)+'</span>'+'<span>'+dates+'</span>'+'</form>';
     newTodo.classList.add('list_item_li')
     todoDiv.appendChild(newTodo);
     //add toDo to local storage
     saveLocal(todoName.value,creationDate,todoCategory.value,todoContent.value,dates);
     //create summary list
+    addSumList(activeCount,archiveCount);
+    //addSumList();
     //edit button
     const editButton = document.createElement('button');
     editButton.innerHTML = '<i class="fas fa-pencil-alt"></i>'
@@ -65,6 +72,17 @@ function addItem(event) {
     todoName.value =""
     todoContent.value =""
     todoDates.value =""
+}
+function addSumList(active,archive) {
+    
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('summary_list_div')
+    const newTodo = document.createElement('li');
+    newTodo.innerHTML=`<form><span>${setIcon(todoCategory.value)} ${todoCategory.value}</span><span>${active}</span><span>${archive}</span></form>`;
+    newTodo.classList.add('summary_list_li')
+    todoDiv.appendChild(newTodo);
+    console.log(todoDiv);
+    SumList.appendChild(todoDiv);
 }
 function getSelectedValue() {
     let selectedValue = document.querySelector(".category_list").value
@@ -93,7 +111,6 @@ function deleteEditArchive(e) {
         todo.remove();
     } else if(item.classList[0]==='edit-btn'){
         const form = todo.firstElementChild;
-        const spanName = form.firstElementChild;
         const spans = form.childNodes;
         spans.forEach(function(currentValue,currentIndex,listObj){
             if(currentIndex==1) ++currentIndex;
@@ -182,6 +199,7 @@ function getTodos() {
     }
     todos.forEach(function(todo){
         //div
+        activeCount++;
         const todoDiv = document.createElement('div');
         todoDiv.classList.add('list_item_div')
         //create todoList
@@ -192,6 +210,13 @@ function getTodos() {
         newTodo.classList.add('list_item_li')
         todoDiv.appendChild(newTodo);
         //create summary list
+        const sumTodoDiv = document.createElement('div');
+        sumTodoDiv.classList.add('summary_list_div')
+        const sumNewTodo = document.createElement('li');
+        sumNewTodo.innerHTML=`<form class="sum_form"><span>${setIcon(todo[2])} ${todo[2]}</span><span>${activeCount}</span><span>${archiveCount}</span></form>`;
+        sumNewTodo.classList.add('summary_list_li')
+        sumTodoDiv.appendChild(sumNewTodo);
+        SumList.appendChild(sumTodoDiv);
         //edit button
         const editButton = document.createElement('button');
         editButton.innerHTML = '<i class="fas fa-pencil-alt"></i>'
@@ -220,12 +245,14 @@ function removeLocalTodos(todo) {
         todos = JSON.parse(localStorage.getItem('todos'));
     }
     const todoIndex = todo.innerText[0];
+    console.log(todo.innerText);
+    console.log(todoIndex);
     for(let i=0; i<todos.length;i++){
         if(todos[i][0]==todoIndex){
             todos.splice(i,1);
             break;
         }
     }
-    return localStorage.setItem('todos', JSON.stringify(todos));
-    //todos.splice(todos.indexOf(todoIndex),1);
+    console.log(todos);
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
